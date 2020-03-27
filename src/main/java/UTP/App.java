4fe8ceb3f18968a7f831fbc84400a5c0b6b06bc7
Kuchczyński <marcin.kuchczynski@atos.net>
2020-03-27 15:23:47 +0100

    - Removed project name + Added authors name to printout

--- src/main/java/UTP/App.java
+++ src/main/java/UTP/App.java
@@ -7,18 +7,20 @@ public final class App {
     private App() {
     }
 
-    private static String projectName = "ChmuryObliczeniowe - Lab 1";
-
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Nazwa projektu: "+ projectName);
         String version = System.getProperty("java.version");
         System.out.println("Java version(JDK): " + version);
         version = System.getProperty("java.runtime.version");
         System.out.println("Java runtime version(JRE): "+version);
+
+        String authorDEV1 = new DEV1().author;
+        System.out.println("Autor DEV1: "+authorDEV1);
+        String authorDEV2 = new DEV2().author;
+        System.out.println("Autor DEV2: "+authorDEV2);
     }
 }
 
