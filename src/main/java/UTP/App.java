17e3192038cedf212f041d42799aa4a549827d73
Kuchczyński <marcin.kuchczynski@atos.net>
2020-03-27 14:58:47 +0100

    Added project name

--- src/main/java/UTP/App.java
+++ src/main/java/UTP/App.java
@@ -7,11 +7,14 @@ public final class App {
     private App() {
     }
 
+    private static String projectName = "ChmuryObliczeniowe - Lab 1";
+
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
+        System.out.println("Nazwa projektu: "+ projectName);
         String version = System.getProperty("java.version");
         System.out.println("Java version(JDK): " + version);
         version = System.getProperty("java.runtime.version");
